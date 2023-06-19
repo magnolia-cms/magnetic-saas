251b891d7a8c6367484b02959dc70a3341173637
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-06-19 14:26:27 +0100

    UPDATE routeTemplate for preview mode

--- nextjs-starter/pages/api/preview.js
+++ nextjs-starter/pages/api/preview.js
@@ -1,10 +1,7 @@
-// More about Preview Mode: https://nextjs.org/docs/advanced-features/preview-mode
-
 export default function handler(req, res) {
-  console.log("preview-api 1.");
-  res.setPreviewData({
-    query: req.query,
-  });
-  console.log("preview-api 2.");
-  res.redirect(req.query.slug);
+	res.setPreviewData({
+		query: req.query,
+	});
+
+	res.redirect(req.query.slug);
 }
