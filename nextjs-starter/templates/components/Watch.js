09e93bf9fcae779b62dda079f5f09aa9aae5eab7
czimmermann <christopher.zimmermann@magnolia-cms.com>
2023-06-25 22:40:46 +0200

    Add watches app and component

---
+++ nextjs-starter/templates/components/Watch.js
@@ -0,0 +1,26 @@
+import A from "../../components/A";
+import Img from "../../components/Img";
+import CTA from "../../components/CTA";
+
+function Watch(props) {
+  const { title, watchLink, transparentBg } = props;
+
+  let cardClassName = "Card";
+
+  if (transparentBg) cardClassName += " transparent";
+
+  return (
+    <div className="Card-wrapper">
+      <div className={cardClassName}>
+        <div>
+          <Img className="Card__image" image={watchLink.image} />
+        </div>
+        <div className="Card__info">
+          <div className="title">{title || watchLink.name}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Watch;
