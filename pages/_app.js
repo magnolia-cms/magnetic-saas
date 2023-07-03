7deccf53b6d7fe062ab29293467f6d9e7d6fcfdc
czimmermann <christopher.zimmermann@magnolia-cms.com>
2023-07-03 19:28:27 +0200

    Test styles per site

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,33 @@
-import '../styles/grid.css';
-import '../styles/globals.css';
-import '../styles/desktop.css';
+import "../styles/grid.css";
+import "../styles/globals.css";
+import "../styles/desktop.css";
+
+import Head from "next/head";
+
+function getStyleSheetLink(siteName) {
+  var cssLink = `/brand-${siteName}.css`;
+  //   if (siteName==="a"){
+  // 	cssLink = "/test.css";
+  //   }=
+  console.log("getStyleSheetLink", cssLink);
+  return cssLink;
+}
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<Component {...pageProps} />
-		</>
-	);
+  // const styleSheetLink = getStyleSheetLink(process.env.HOSTNAME);
+  const styleSheetLink = getStyleSheetLink(
+    process.env.NEXT_PUBLIC_MGNL_SITE_NAME
+  );
+
+  return (
+    <>
+      <Head>
+        <link href={styleSheetLink} rel="stylesheet" type="text/css" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
