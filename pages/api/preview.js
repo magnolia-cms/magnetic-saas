8ffbe2fdf550d98dfcba8225120b3363eba52afa
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-07-11 10:14:17 +0100

    UPDATE preview and baseURL

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,30 +1,21 @@
-import { spaRootNodePath } from "../../utils/api";
+import { spaRootNodePath } from '../../utils/api';
 
 const setCookieSameSite = (res) => {
-  const cookies = res.getHeader("Set-Cookie");
-  res.setHeader(
-    "Set-Cookie",
-    cookies?.map((cookie) =>
-      cookie.replace("SameSite=Lax", "SameSite=None;Secure")
-    )
-  );
+	const cookies = res.getHeader('Set-Cookie');
+	res.setHeader(
+		'Set-Cookie',
+		cookies?.map((cookie) =>
+			cookie.replace('SameSite=Lax', 'SameSite=None;Secure')
+		)
+	);
 };
 
 export default function handler(req, res) {
-  res.setPreviewData({
-    query: req.query,
-  });
+	res.setPreviewData({
+		query: req.query,
+	});
 
-  setCookieSameSite(res);
+	setCookieSameSite(res);
 
-  let nodePath = req.query.slug;
-
-  // if (spaRootNodePath) {
-  // 	if (!nodePath.startsWith(spaRootNodePath)) {
-  // 	  nodePath = `${spaRootNodePath}${nodePath}`;
-  // 	}
-  //   }
-  console.log("Preview.query:", req.query);
-
-  res.redirect("/");
+	res.redirect(req.query.slug);
 }
